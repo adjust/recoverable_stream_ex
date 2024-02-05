2323ab2057ea3f1c0a48afaf47ed0c42ee08f0c4
Jay Rogov <jay.rogov@adjust.com>
2024-02-05 19:41:15 +0100

    BREAKING CHANGE: Remove support for Elixir 1.6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule RecoverableStreamEx.MixProject do
     [
       app: :recoverable_stream_ex,
       version: "1.0.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
